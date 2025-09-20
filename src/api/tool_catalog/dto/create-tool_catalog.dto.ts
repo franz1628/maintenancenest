a73bc8de1b1f69d33a5017f0650242586046e6aa
Franz Wilder <gualambo@gmail.com>
2025-09-20 11:24:24 -0500

    refactor(CreateToolCatalogDto): add validation decorators for name, description, photo, and state properties

--- src/api/tool_catalog/dto/create-tool_catalog.dto.ts
+++ src/api/tool_catalog/dto/create-tool_catalog.dto.ts
@@ -1,6 +1,13 @@
+import { IsInt, IsString, MinLength } from "class-validator";
+
 export class CreateToolCatalogDto {
+  @IsString()
+  @MinLength(3)
   name: string;
+  @IsString()
   description?: string;
+  @IsString()
   photo?: string;
+  @IsInt()
   state?: number;
 }
